2678a013661598cc2551872dfd4a84fc15c7dd6c
Nattapong Ousirimaneechai <chai-best@hotmail.com>
2019-10-31 12:16:40 +0700

    Add: sol3

---
+++ PreFirstRound/3/3.py
@@ -0,0 +1,20 @@
+def solution(A):
+    n_dup_cur = 1
+    cur, before = A[0], None
+    pair = [1, 0] # [n_cur, n_before]
+    max_bi_valued_length = 1
+    for i in range(1, len(A)):
+        if A[i] == cur:
+            n_dup_cur += 1
+            pair[0] += 1
+        elif A[i] == before:
+            before, cur = cur, before
+            pair = [pair[1] + 1, pair[0]]
+            n_dup_cur = 1
+        else:
+            before, cur = cur, A[i]
+            pair = [1, n_dup_cur]
+            n_dup_cur = 1
+        if pair[0] + pair[1] > max_bi_valued_length:
+            max_bi_valued_length = pair[0] + pair[1]
+    return max_bi_valued_length
