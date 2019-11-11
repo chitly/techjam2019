326a1ae4790b7193ad410dfdfaf4cf85fe4d471d
Krit Gangwanpongpun <ac127.44264@gmail.com>
2019-11-11 23:01:39 +0700

    no message

--- DeepCode/FirstRound/Task3/3.py
+++ DeepCode/FirstRound/Task3/3.py
@@ -27,27 +27,51 @@ for i in range(1, A + 1):
     table1.append(int(input()))
 B = int(input())
 line = list()
+line.append(table1[0])
 line2 = list()
 line2.append(1)
-line.append(table1[0])
 for i in range(1, B + 1):
-  tmp = int(input())
-  line.append(line[i-1]+tmp)
+    tmp = int(input())
+    line.append(line[i-1]+tmp)
+    if line2[i-1] <= 0 or tmp < 0:
+        line2.append(0)
+    else:
+        line2.append(1)
+
+# for i in line2:
+#   print(i, end = ' ')
 
 for i in range(1, A):
-  for j in line:
-    print(j, end = ' ')
-  print()
-  line[0] = table1[i]
-  for j in line:
-    print(j, end = ' ')
-  print()
-  for j in range(1, B + 1):
-    line[j] = line[0] + line[j]
-    print(str(line[j]) + ' ', end='')
-  print()
+    # for j in line:
+    #   print(j, end = ' ')
+    # print()
+    line[0] = table1[i]
+    # for j in line:
+    #   print(j, end = ' ')
+    # print()
+    if line[i] < 0:
+        line2[0] = 0
+    else:
+        line2[0] = 1
+    for j in range(1, B + 1):
+        line[j] = line[0] + line[j]
+        # if line[j] >=0:
+        #   line2[0] = 1
+        # else:
+        #   line2[0] = 0
 
-  
+        line2[j] = (line2[j] + line2[j-1]) % 1000000007
+        # print(str(line[j]) + ' ', end='')
+        # print('line2 ==> ', end = ' ')
+    #   for j in line2:
+    #     print(j, end = ' ')
+    #   print()
+    # print()
+
+# print()
+
+print(line2[len(line2)-1])
+
 
 # for i in range(0, A):
 #   if i == 0:
